fd673c69af970db291f652df8c1fe8f57bbe20de
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2026-03-30 00:15:12 +0900

    Vitest の設定ファイル追加

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'edge-runtime',
+    globals: true,
+    include: ['test/**/*.test.ts'],
+    coverage: {
+      reporter: ['text', 'html'],
+    },
+  },
+  resolve: {
+    tsconfigPaths: true,
+  },
+});
