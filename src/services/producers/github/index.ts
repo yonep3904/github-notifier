26fb8be60745be874f9868deab3ebbf0cdf8e5b8
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2026-04-03 03:36:02 +0900

    index.ts を追加

---
+++ src/services/producers/github/index.ts
@@ -0,0 +1,7 @@
+export { GithubWebhookParser } from "./parser";
+export { GithubNotificationProducer } from "./producer";
+export {
+  SupportedEvent,
+  SupportedEventName,
+  supportedEventList,
+} from "./supported-github-events";
