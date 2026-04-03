fa8b446f85c2227593c9730197ad2e858805acaa
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2026-04-03 23:26:31 +0900

    環境変数の型を追加

--- src/types/env.ts
+++ src/types/env.ts
@@ -1,5 +1,20 @@
 import type { NotificationJob } from "@/types/internal/pipeline";
 
 export type Env = CloudflareBindings & {
+  // Queue
   NOTIFICATION_QUEUE: Queue<NotificationJob>;
+
+  // Environment variable
+  GITHUB_WEBHOOK_SECRET?: string;
+  MANUAL_NOTIFICATION_PASSWORD?: string;
+  DISCORD_WEBHOOK_URL_1?: string;
+  DISCORD_WEBHOOK_URL_2?: string;
+  DISCORD_WEBHOOK_URL_3?: string;
+  DISCORD_WEBHOOK_URL_4?: string;
+  DISCORD_WEBHOOK_URL_5?: string;
+  SLACK_WEBHOOK_URL_1?: string;
+  SLACK_WEBHOOK_URL_2?: string;
+  SLACK_WEBHOOK_URL_3?: string;
+  SLACK_WEBHOOK_URL_4?: string;
+  SLACK_WEBHOOK_URL_5?: string;
 };
