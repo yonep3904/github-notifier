4cb70e0fc7d2ca81bbb1127d48215a306c9dd410
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2026-04-04 23:15:39 +0900

    テスト用のenvを追加

---
+++ test/helpers/env.ts
@@ -0,0 +1,27 @@
+import type { Env } from "@/types/env";
+
+const defaultTestEnv: Env = {
+  // Queue
+  NOTIFICATION_QUEUE: {} as Env["NOTIFICATION_QUEUE"],
+
+  // Environment variable
+  GITHUB_WEBHOOK_SECRET: "testsecret",
+  MANUAL_NOTIFICATION_PASSWORD: "testpassword",
+  DISCORD_WEBHOOK_URL_1: "https://discord.com/api/webhooks/testwebhook1",
+  DISCORD_WEBHOOK_URL_2: "https://discord.com/api/webhooks/testwebhook2",
+  DISCORD_WEBHOOK_URL_3: "https://discord.com/api/webhooks/testwebhook3",
+  DISCORD_WEBHOOK_URL_4: "https://discord.com/api/webhooks/testwebhook4",
+  DISCORD_WEBHOOK_URL_5: "https://discord.com/api/webhooks/testwebhook5",
+  SLACK_WEBHOOK_URL_1: "https://hooks.slack.com/services/test/slack/webhook1",
+  SLACK_WEBHOOK_URL_2: "https://hooks.slack.com/services/test/slack/webhook2",
+  SLACK_WEBHOOK_URL_3: "https://hooks.slack.com/services/test/slack/webhook3",
+  SLACK_WEBHOOK_URL_4: "https://hooks.slack.com/services/test/slack/webhook4",
+  SLACK_WEBHOOK_URL_5: "https://hooks.slack.com/services/test/slack/webhook5",
+};
+
+export function createTestEnv(overrides: Partial<Env> = {}): Env {
+  return {
+    ...defaultTestEnv,
+    ...overrides,
+  };
+}
