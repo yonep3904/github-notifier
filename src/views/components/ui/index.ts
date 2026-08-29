38ad96ede0fe90387eafdfb301fda1a833278ed3
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2026-08-30 04:47:30 +0900

    Card を追加

--- src/views/components/ui/index.ts
+++ src/views/components/ui/index.ts
@@ -1,3 +1,4 @@
 export { Badge, type BadgeProps } from "./Badge";
+export { Card, type CardProps } from "./Card";
 export { CheckFlag, type CheckFlagProps } from "./CheckFlag";
 export { LucideIcon, type LucideIconProps } from "./LucideIcon";
