afa340bb424f4f4caea9d96cea97ab2f9b35dc5a
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2026-08-30 05:18:24 +0900

    IssueCard を追加

--- src/views/components/status/index.ts
+++ src/views/components/status/index.ts
@@ -1,3 +1,4 @@
+export { IssueCard, type IssueCardProps } from "./IssueCard";
 export { IssueList, type IssueListProps } from "./IssueList";
 export { MetricCard, type MetricCardProps } from "./MetricCard";
 export { StatusHero, type StatusHeroProps } from "./StatusHero";
