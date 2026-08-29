0b529b143da4c26963118f56511da3912507dd22
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2026-08-30 05:17:39 +0900

    MetricCard を追加

--- src/views/components/status/index.ts
+++ src/views/components/status/index.ts
@@ -1,2 +1,3 @@
 export { IssueList, type IssueListProps } from "./IssueList";
+export { MetricCard, type MetricCardProps } from "./MetricCard";
 export { StatusHero, type StatusHeroProps } from "./StatusHero";
